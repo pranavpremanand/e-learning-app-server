302c6448f4fcb4e750a7b68de9c22bceec67d88a
Pranav Premanand <mpranavprem@gmail.com>
2023-04-13 12:11:20 +0530

    added changes in app.js

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 import express from "express";
 import cors from "cors";
-import "./config/connection.js";
 import route from "./routes/route.js";
 import dotenv from "dotenv";
 dotenv.config();
-import mongoose from "./config/connection.js";
+import connection from "./config/connection.js";
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
 app.use("/", route);
 app.listen(process.env.PORT, () =>
-  console.log("Server is running on port :", process.env.PORT)
+  console.log(`Server is running on port :${process.env.PORT}`)
 );
 
 export default app;
