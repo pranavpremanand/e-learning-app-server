249b8155d2054df3f0c513e3ead2f2f9e1a66c12
Pranav Premanand <mpranavprem@gmail.com>
2023-04-13 11:42:52 +0530

    imported connection to db in app.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import "./config/connection.js";
 import route from './routes/route.js'
+import './config/connection.js'
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
